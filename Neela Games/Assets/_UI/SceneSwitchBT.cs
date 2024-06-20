97cb120474f9faafe2a93c54e22af7c1fafb18a6
yashwankhade <yashwankhade987@gmail.com>
2024-06-20 13:56:18 +0530

    Level 5 and 6 done

--- Neela Games/Assets/_UI/SceneSwitchBT.cs
+++ Neela Games/Assets/_UI/SceneSwitchBT.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 namespace NeelaGames.AnimalRescue {
 
     public class SceneSwitchBT : MonoBehaviour
     {
         public int sceneIndexToLoad;
+        public bool loadNextScene;
 
         public void OnButtonClicked()
         {
+            if (loadNextScene)
+            {
+                if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCountInBuildSettings - 1)
+                {
+                    GameManager.Instance.LoadLevel(0);
+                    return;
+                }
+                GameManager.Instance.LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
+                return;
+            }
             GameManager.Instance.LoadLevel(sceneIndexToLoad);
         }
 
